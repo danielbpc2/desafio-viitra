09eb76ecc96eca1b9c8544d93183cf2ab1f491aa
danielbpc2 <danielbpc2@hotmail.com>
2020-09-18 12:09:11 -0300

    Feature and Style: Select fields, general css, Background image change
    Select fields now working with IBGE API

--- frontend/src/pages/Register/index.tsx
+++ frontend/src/pages/Register/index.tsx
@@ -2,10 +2,16 @@ import React, { ChangeEvent, FormEvent, useState, useEffect } from "react";
 import logo from "../../assets/viitrafio.svg";
 import "./styles.css";
 
+import { FiUpload } from "react-icons/fi";
+
 import { Link } from "react-router-dom";
 import axios from "axios";
 
 interface UFResponse {
+  nome: string;
+  sigla: string;
+}
+interface CityResponse {
   nome: string;
 }
 
@@ -18,16 +24,35 @@ const Register = () => {
     address: "",
   });
 
-  const [ufs, setUfs] = useState([]);
-  const [cities, setCities] = useState([]);
+  const [ufs, setUfs] = useState<string[][]>([]);
+  const [cities, setCities] = useState<string[]>([]);
+  const [selectedUf, setSelectedUf] = useState("0");
+  const [selectedCity, setSelectedCity] = useState("0");
 
   useEffect(() => {
     axios
-      .get<UFResponse>(
+      .get<UFResponse[]>(
         "https://servicodados.ibge.gov.br/api/v1/localidades/estados/"
       )
-      .then((data) => console.log(data));
+      .then((response) => {
+        const ufArray = response.data.map((uf) => [uf.sigla, uf.nome]);
+        setUfs(ufArray);
+      });
   }, []);
+
+  useEffect(() => {
+    if (selectedUf === "0") return;
+
+    axios
+      .get<CityResponse[]>(
+        `https://servicodados.ibge.gov.br/api/v1/localidades/estados/${selectedUf}/municipios`
+      )
+      .then((response) => {
+        const citiesArray = response.data.map((city) => city.nome);
+
+        setCities(citiesArray);
+      });
+  }, [selectedUf]);
 
   function handleRegister(event: FormEvent) {
     event.preventDefault();
@@ -36,6 +61,17 @@ const Register = () => {
   function handleChange(event: ChangeEvent<HTMLInputElement>) {
     const { name, value } = event.target;
     setFormdata({ ...formData, [name]: value });
+  }
+
+  function handleSelectUf(event: ChangeEvent<HTMLSelectElement>) {
+    const uf = event.target.value;
+
+    setSelectedUf(uf);
+  }
+  function handleSelectCity(event: ChangeEvent<HTMLSelectElement>) {
+    const city = event.target.value;
+
+    setSelectedCity(city);
   }
 
   return (
@@ -115,18 +151,38 @@ const Register = () => {
                 </div>
                 <div className="field-group">
                   <div className="field">
-                    <select name="uf">
+                    <select
+                      value={selectedUf}
+                      onChange={handleSelectUf}
+                      name="uf"
+                    >
                       <option value="0">Selecione o seu Estado</option>
+                      {ufs.map((uf) => (
+                        <option key={uf[0]} value={uf[0]}>
+                          {uf[1]}
+                        </option>
+                      ))}
                     </select>
                   </div>
                   <div className="field">
-                    <select name="city">
+                    <select
+                      onChange={handleSelectCity}
+                      value={selectedCity}
+                      name="city"
+                    >
                       <option value="0">Selecione sua Cidade</option>
+                      {cities.map((city) => (
+                        <option key={city} value={city}>
+                          {city}
+                        </option>
+                      ))}
                     </select>
                   </div>
                 </div>
                 <button type="submit" className="register-button">
-                  <span>icon</span>
+                  <span>
+                    <FiUpload />
+                  </span>
                   <strong>Cadastrar</strong>
                 </button>
                 ou
